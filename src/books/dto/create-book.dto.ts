3e76fc10f533afc39aafc585cff8d5d6d6c28ee8
DemendiAkos <akosdemendi@gmail.com>
2024-10-25 15:49:06 +0200

    fix: validation enhanced

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -12,6 +12,5 @@ export class CreateBookDto {
 
     @IsNumber()
     publishYear: number;
-
-
+    
 }
--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+    @IsBoolean()
+    @IsOptional()
+    reserved?: boolean;
+}
